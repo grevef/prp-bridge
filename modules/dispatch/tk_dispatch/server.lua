c61998f482ded0e9bb41997bf147979fb8f354a4
Epixx1337 <achrafhoufaty@gmail.com>
2026-03-26 17:19:35 +0100

    Update source link
    My bad used an old link

--- modules/dispatch/tk_dispatch/server.lua
+++ modules/dispatch/tk_dispatch/server.lua
@@ -7,7 +7,7 @@ local dispatch = {}
 ---@param blip AlertBlip
 ---@param alertFlash? boolean
 function dispatch.sendAlert(src, jobs, coords, data, blip, alertFlash)
-    -- Source: https://docs.tkscripts.xyz/tk_dispatch
+    -- Source: https://tk-scripts.gitbook.io/docs/tk_dispatch/exports
     exports.tk_dispatch:addCall({
         title = data.title,
         code = data.code,
